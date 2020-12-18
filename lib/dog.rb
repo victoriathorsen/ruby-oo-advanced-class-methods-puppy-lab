ce858e0720f0bfc70f19919fa8f4d05f83872565
Victoria Thorsen <victoriathorsenn@gmail.com>
2020-12-17 20:44:51 -0800

    commit tree f666406d11880d125a84dc74893eb2777fd95ab2

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog
+    attr_accessor :name
+    @@all = []
+
+    def initialize (name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def  self.clear_all
+        self.all.clear
+    end
+
+    def self.print_all
+        self.all.each do |dogs|
+        puts dogs.name
+        end
+    end
+
+    def save
+        @@all << self
+    end
+
+end
